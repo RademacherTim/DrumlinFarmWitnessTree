78a9507311e272434412afc720e1f5c31652e912
Tim Rademacher <rademacher.tim@gmail.com>
2022-06-02 16:49:08 -0400

    Updated formatting of checkCommunity.R file.

--- rScripts/checkCommunity.R
+++ rScripts/checkCommunity.R
@@ -10,28 +10,26 @@
 #   4)  explainGallWasps                        Between 1st of Sep and 30th of Nov
 #----------------------------------------------------------------------------------------
 
-# Explain seed dispersal
-#----------------------------------------------------------------------------------------
-explainSeedDispersal <- function (ptable, TEST = 0) {
-  if (substring (Sys.Date (), 6, 10) > '09-01' & substring (Sys.Date (), 6, 10) < '11-15'| 
+# Explain seed dispersal ----------------------------------------------------------------
+explainSeedDispersal <- function(ptable, TEST = 0) {
+  if (substring(Sys.Date(), 6, 10) > '09-01' & substring(Sys.Date(), 6, 10) < '11-15'| 
       TEST == 1) {
-    postDetails <- getPostDetails ("explainSeedDispersal")
-    message   <- sprintf (postDetails [["MessageText"]])
-    expiryDate <- sprintf ("%s-11-30 23:59:59 %s", 
-                           format (Sys.Date (), format = '%Y'),
-                           treeTimeZone) %>% lubridate::as_datetime (tz = treeTimeZone)
-    ptable    <- add_row (ptable, 
-                          priority    = postDetails [["Priority"]],
-                          fFigure     = postDetails [['fFigure']],
-                          figureName  = postDetails [["FigureName"]], 
-                          message     = message, 
-                          hashtags    = postDetails [["Hashtags"]], 
-                          expires     = expiryDate)
+    postDetails <- getPostDetails("explainSeedDispersal")
+    message   <- sprintf(postDetails [["MessageText"]])
+    expiryDate <- sprintf("%s-11-30 23:59:59 %s", 
+                          format(Sys.Date(), format = '%Y'),
+                          treeTimeZone) %>% lubridate::as_datetime(tz = treeTimeZone)
+    ptable <- add_row(ptable, 
+                      priority    = postDetails [["Priority"]],
+                      fFigure     = postDetails [['fFigure']],
+                      figureName  = postDetails [["FigureName"]], 
+                      message     = message, 
+                      hashtags    = postDetails [["Hashtags"]], 
+                      expires     = expiryDate)
   } 
   
-  # Return table with posts
-  #--------------------------------------------------------------------------------------
-  return (ptable)
+  # Return table with posts -------------------------------------------------------------
+  return(ptable)
 } 
 
 # Check for wildlife images (visitors) at the tree
@@ -45,108 +43,98 @@ explainSeedDispersal <- function (ptable, TEST = 0) {
 # , where XXXX has to be replaced by an increasing number with preceeding zeros. The 
 #   first image would be named wildlifeCameraImage0001.jpg and so on. 
 #----------------------------------------------------------------------------------------
-checkCommunityWildlife <- function (ptable, TEST = 0) {
+checkCommunityWildlife <- function(ptable, TEST = 0) {
   
-  # Check whether there is a new wildlife photo
-  #--------------------------------------------------------------------------------------
-  listOfVisitors <- list.files (path = sprintf ('%simages/wildlifeCam/',path), 
+  # Check whether there is a new wildlife photo -----------------------------------------
+  listOfVisitors <- list.files (path = sprintf('%simages/wildlifeCam/',path), 
                                 pattern = '.jpg')  
-  if (file.exists (paste0 (path, 'code/memory.csv'))) {
-    memory <- read_csv (file = paste0 (path, 'code/memory.csv'), col_types = cols ())
+  if (file.exists(paste0(path, 'code/memory.csv'))) {
+    memory <- read_csv(file = paste0(path, 'code/memory.csv'), col_types = cols())
   } else {
-    stop (paste0 (Sys.time (), '; checkCommunity.R; Error: There is not memory file'))
+    stop(paste0(Sys.time(), '; checkCommunity.R; Error: There is not memory file'))
   }
   
   
-  # Check that there is at least one picture in the directory
-  #--------------------------------------------------------------------------------------
-  if (length (listOfVisitors) > 0 | TEST >= 1) {
+  # Check that there is at least one picture in the directory ---------------------------
+  if (length(listOfVisitors) > 0 | TEST >= 1) {
     
-    # Check whether there is a new picture in the directory
-    #------------------------------------------------------------------------------------
-    if (as.numeric (substring (tail (listOfVisitors, n = 1), 20, 23)) > 
+    # Check whether there is a new picture in the directory -----------------------------
+    if (as.numeric(substring(tail(listOfVisitors, n = 1), 20, 23)) > 
         memory [['numberOfPreviousVisitors']] | TEST >= 1) {
       
-      # Get message depending on time of year
-      #----------------------------------------------------------------------------------
-      if (substring (Sys.Date (), 6, 10) >  '03-21' & 
-          substring (Sys.Date (), 6, 10) <= '06-21' | 
+      # Get message depending on time of year -------------------------------------------
+      if (substring(Sys.Date(), 6, 10) >  '03-21' & 
+          substring(Sys.Date(), 6, 10) <= '06-21' | 
           TEST == 1) { # it is spring
-        postDetails <- getPostDetails ("checkCommunityWildlife - spring")
-      } else if (substring (Sys.Date (), 6, 10) >  '06-21' & 
-                 substring (Sys.Date (), 6, 10) <= '09-21' | 
+        postDetails <- getPostDetails("checkCommunityWildlife - spring")
+      } else if (substring(Sys.Date(), 6, 10) >  '06-21' & 
+                 substring(Sys.Date(), 6, 10) <= '09-21' | 
                  TEST == 2) { # it is summer
-        postDetails <- getPostDetails ("checkCommunityWildlife - summer")
-      } else if (substring (Sys.Date (), 6, 10) >  '09-21' & 
-                 substring (Sys.Date (), 6, 10) <= '11-21' | 
+        postDetails <- getPostDetails("checkCommunityWildlife - summer")
+      } else if (substring(Sys.Date(), 6, 10) >  '09-21' & 
+                 substring(Sys.Date(), 6, 10) <= '11-21' | 
                  TEST == 3) { # it is fall
-        postDetails <- getPostDetails ("checkCommunityWildlife - fall")
-      } else if (substring (Sys.Date (), 6, 10) >  '11-21' & 
-                 substring (Sys.Date (), 6, 10) <= '03-21' | 
+        postDetails <- getPostDetails("checkCommunityWildlife - fall")
+      } else if (substring(Sys.Date(), 6, 10) >  '11-21' & 
+                 substring(Sys.Date(), 6, 10) <= '03-21' | 
                  TEST == 4) { # it is winter
-        postDetails <- getPostDetails ("checkCommunityWildlife - winter")
+        postDetails <- getPostDetails("checkCommunityWildlife - winter")
       }
-      message <- sprintf (postDetails [["MessageText"]])
-      delay   <- as.numeric (substring (postDetails [['ExpirationDate']], 7, 8)) * 60 * 60
-      ptable  <- add_row (ptable, 
-                          priority    = postDetails [["Priority"]],
-                          fFigure     = postDetails [['fFigure']],
-                          figureName  = paste0 (path,'images/wildlifeCam/',
-                                                tail (listOfVisitors, n = 1)),
-                          message     = message, 
-                          hashtags    = postDetails [["Hashtags"]], 
-                          expires     = expiresIn (delay = delay))
+      message <- sprintf(postDetails [["MessageText"]])
+      delay   <- as.numeric(substring(postDetails [['ExpirationDate']], 7, 8)) * 60 * 60
+      ptable  <- add_row(ptable, 
+                         priority   = postDetails [["Priority"]],
+                         fFigure    = postDetails [['fFigure']],
+                         figureName = paste0(path,'images/wildlifeCam/',
+                                             tail(listOfVisitors, n = 1)),
+                          message   = message, 
+                          hashtags  = postDetails [["Hashtags"]], 
+                          expires   = expiresIn(delay = delay))
       
-      # increase the wildlife counter in the memory
-      #----------------------------------------------------------------------------------
+      # increase the wildlife counter in the memory -------------------------------------
       memory [['numberOfPreviousVisitors']] <- memory [['numberOfPreviousVisitors']] + 1
-      write_csv (memory, paste0 (path,'code/memory.csv'))
+      write_csv(memory, paste0(path,'code/memory.csv'))
     }
   }
   
-  # Return table with posts
-  #--------------------------------------------------------------------------------------
-  return (ptable)
+  # Return table with posts -------------------------------------------------------------
+  return(ptable)
 } 
 
-# Explain gypsy moth herbivory
-#----------------------------------------------------------------------------------------
-explainGypsyMothHerbivory <- function (ptable, TEST = 0) {
-  if (substring (Sys.Date (), 6, 10) > '05-15' & substring (Sys.Date (), 6, 10) < '08-31'| 
+# Explain gypsy moth herbivory ----------------------------------------------------------
+explainGypsyMothHerbivory <- function(ptable, TEST = 0) {
+  if (substring(Sys.Date(), 6, 10) > '05-15' & substring(Sys.Date(), 6, 10) < '08-31'| 
       TEST == 1) {
-    postDetails <- getPostDetails ("explainGypsyMothHerbivory")
-    ptable    <- add_row (ptable, 
-                          priority    = postDetails [["Priority"]],
-                          fFigure     = postDetails [['fFigure']],
-                          figureName  = postDetails [["FigureName"]], 
-                          message     = postDetails [["MessageText"]], 
-                          hashtags    = postDetails [["Hashtags"]], 
-                          expires     = expiresIn (0))
+    postDetails <- getPostDetails("explainGypsyMothHerbivory")
+    ptable    <- add_row(ptable, 
+                         priority   = postDetails [["Priority"]],
+                         fFigure    = postDetails [['fFigure']],
+                         figureName = postDetails [["FigureName"]], 
+                         message    = postDetails [["MessageText"]], 
+                         hashtags   = postDetails [["Hashtags"]], 
+                         expires    = expiresIn(0))
   } 
   
-  # Return table with posts
-  #--------------------------------------------------------------------------------------
-  return (ptable)
+  # Return table with posts -------------------------------------------------------------
+  return(ptable)
 } 
 
-# Explain gall wasps on oak trees
-#----------------------------------------------------------------------------------------
-explainGallWasps <- function (ptable, TEST = 0) {
-  if (substring (Sys.Date (), 6, 10) > '09-01' & substring (Sys.Date (), 6, 10) < '11-15'| 
+# Explain gall wasps on oak trees -------------------------------------------------------
+explainGallWasps <- function(ptable, TEST = 0) {
+  if (substring(Sys.Date(), 6, 10) > '09-01' & substring(Sys.Date(), 6, 10) < '11-15'| 
       TEST == 1) {
-    postDetails <- getPostDetails ("explainGallWasps")
-    message   <- sprintf (postDetails [["MessageText"]])
-    ptable    <- add_row (ptable, 
-                          priority    = postDetails [["Priority"]],
-                          fFigure     = postDetails [['fFigure']],
-                          figureName  = postDetails [["FigureName"]], 
-                          message     = message, 
-                          hashtags    = postDetails [["Hashtags"]], 
-                          expires     = expiresIn (0))
+    postDetails <- getPostDetails("explainGallWasps")
+    message <- sprintf(postDetails [["MessageText"]])
+    ptable  <- add_row(ptable, 
+                       priority   = postDetails [["Priority"]],
+                       fFigure    = postDetails [['fFigure']],
+                       figureName = postDetails [["FigureName"]], 
+                       message    = message, 
+                       hashtags   = postDetails [["Hashtags"]], 
+                       expires    = expiresIn(0))
   } 
   
-  # Return table with posts
-  #--------------------------------------------------------------------------------------
-  return (ptable)
+  # Return table with posts -------------------------------------------------------------
+  return(ptable)
 } 
 #========================================================================================
